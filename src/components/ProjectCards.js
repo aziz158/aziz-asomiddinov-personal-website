f3ff505df7efd9649e45d4c56b78c3002497314f
Aziz Asomiddinov <mi6xino@gmail.com>
2021-02-05 18:28:35 -0800

    Updated the Project page with cards

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,45 +1,44 @@
 import React from 'react';
 import './ProjectCards.css';
-import CardItem from './CardItem';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
-function ProjectCards() {
+import { Card } from "react-bootstrap";
+
+const ProjectCards = () => {
+  const cardInfo = [
+    
+    {
+      image: "https://raw.githubusercontent.com/lmlstrandingnetwork/lml-stranding-map/master/public/screenshots/screenshot01.png",
+      title: "Marine Stranding Map Web App",
+      text: "Checkout Marine Stranding Web Application tool that we've built for UCSC Long Marine Lab scientist. \nTechnologies used: ReactJS, Bootstrap, Algolia, Mapbox, Google Firebase, Heroku",
+    },
+    {
+      image:
+        "https://raw.githubusercontent.com/hamfox/save-our-species/master/Assets/login-view.png",
+      title: "Save our Species iOS app",
+      text: "See our IPhone application for reporting Strandings on the beaches \nTechnologies used: Swift, Google Firebase",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/8/82/Huffman_tree_2.svg",
+      title: "Huffman Enconding Project",
+      text: "This is the project that I was working in my freshmen year at De Anza. I used Huffman encoding algorithm to encrypt and compress the data \nTechnologies used: C++, Binary Tree, Linked List, Dictionary",
+    },
+  ];
+
+  const renderCard = (card, index) => {
     return (
-      <div className='pcards'>
-        <h1>My Projects</h1>
-        <div className='pcards__container'>
-          <div className='pcards__wrapper'>
-            <ul className='pcards__items'>
-              <CardItem
-                src='./images/Desktop.jpg'
-                text='UCSC Long Marine Lab Stranding Map'
-                label='Web-App'
-                path='portfolio'
-              />
-              <CardItem
-                src='./images/Projects.jpeg'
-                text='Take a look at the projects that Ive been part of'
-                label='Mobile-App'
-                path='/projects'
-              />
-            </ul>
-            <ul className='pcards__items'>
-              <CardItem
-                src='./images/IMG_1281.jpg'
-                text='Learn something new about me'
-                label='About me'
-                path='/aboutme'
-              />
-              <CardItem
-                src='./images/Email.jpg'
-                text='Feel free to send me an email or connect me in social media'
-                label='Contact me'
-                path='/contacts'
-              />
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Card style={{ width: "18rem" }} key={index} className="box">
+        <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+        <Card.Body>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>{card.text}</Card.Text>
+        </Card.Body>
+      </Card>
     );
-  }
-  
-  export default ProjectCards;
+  };
+
+  return <div className="grid">{cardInfo.map(renderCard)}</div>;
+};
+
+export default ProjectCards;
