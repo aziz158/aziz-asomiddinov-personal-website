a86ba7176ae38592e66929ca8c8dd62fc2023d56
Aziz Asomiddinov <mi6xino@gmail.com>
2021-02-10 16:26:37 -0800

    Installed new dependencies and changes to Portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Document, Page} from 'react-pdf';
 import './Portfolio.css';
+import Resume from '../../files/Aziz2021_Resume.pdf';
 
 function Portfolio() {
-  return <h1 className='portfolio'>PORTFOLIO</h1>;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+
+  return(
+  <div className='portfolio'>
+    <div>
+      <Document
+        file={Resume}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
+    </div>
+  </div>);
 }
 
 export default Portfolio;
