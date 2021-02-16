e4266c9f3fefe16b9555d75084d2183c43da14ba
Aziz Asomiddinov <mi6xino@gmail.com>
2021-02-16 15:51:54 -0800

    added ProfileCard component to the Hero Section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
+import ProfileCard from './ProfileCard';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='./videos/IMG_1335.mp4' autoPlay loop muted />
-      <h1>Hello there</h1>
-      <p>Welcome to my project</p>
+      <ProfileCard/>
     </div>
   );
 }
