4f5948856c0622e22a3a6910b804e519fb0db67d
Aziz Asomiddinov <mi6xino@gmail.com>
2021-02-16 17:23:13 -0800

    added new LinkedIn badge in contact me page

--- src/components/pages/Contacts.js
+++ src/components/pages/Contacts.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import '../../App.css';
+import { Helmet } from 'react-helmet';
+import './Contacts.css';
 
-export default function Contacts() {
-  return <h1 className='contacts'>CONTACTS</h1>;
+
+function Contacts() {
+
+  return (
+      <div className='contacts'>
+        <Helmet>
+          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+        </Helmet>
+        <div className = 'text'> Connect with me on LinkedIn
+          <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="azizkhuja"><a class="LI-simple-link" href='https://www.linkedin.com/in/azizkhuja?trk=profile-badge'>Azizkhuja Asomiddinov</a></div>
+        Send me an email at azizkhuja.asomiddinov562@gmail.com
+        </div>
+      </div>
+    )
 }
+
+export default Contacts;
