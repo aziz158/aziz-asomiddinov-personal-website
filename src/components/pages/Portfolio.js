1bf1a3d8a4324d3f7c6ac48f02ac599c195b0595
Aziz Asomiddinov <mi6xino@gmail.com>
2021-02-17 16:36:21 -0800

    Portfolio page complete

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,30 +1,12 @@
 import React, {useState} from 'react';
-import {Page} from 'react-pdf';
-import { Document } from 'react-pdf/dist/umd/entry.parcel';
 import './Portfolio.css';
-import Resume from '../../files/Aziz2021_Resume.pdf';
-
-const external_pdf = 'http://www.africau.edu/images/default/sample.pdf';
+import Resume from "../../files/Aziz2021_Resume1024_1.jpg"
 
 function Portfolio() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-
   return(
-  <div className='portfolio'>
-    <div>
-      <Document
-        file={Resume}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>Page {pageNumber} of {numPages}</p>
+  <div className='portfolio'>  
+    <div className='portfolio-image-container'>
+      <img src={Resume} alt = 'Resume Image'/>
     </div>
   </div>);
 }
